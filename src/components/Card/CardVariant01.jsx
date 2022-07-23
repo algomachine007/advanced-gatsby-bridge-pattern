c1136314308710104dd85a0ad6863dd227fb0528
Amaben2020 <uzochukwubenamara@gmail.com>
2022-07-23 22:10:22 +0100

    moving the modal to hero variant 01

--- src/components/Card/CardVariant01.jsx
+++ src/components/Card/CardVariant01.jsx
@@ -1,43 +1,10 @@
 import React from "react"
-import Modal from "@/components/Modal/Modal"
-import useModal from "./../../hooks/useModal"
+
 import Tabs from "@/components/Tabs/Tabs"
 
 const CardVariant01 = props => {
-  const { isOpen, handleStopPropagation, toggleModal, transitions } = useModal()
-  console.log("props", props)
   return (
     <div style={{ border: "2px solid red" }}>
-      {/* {props?.title}
-      CardVariant01 */}
-
-      <button onClick={toggleModal}>Open Modal</button>
-      <Modal
-        isOpen={isOpen}
-        handleStopPropagation={handleStopPropagation}
-        toggleModal={toggleModal}
-        transitions={transitions}
-      >
-        Ich spreche Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Error, nobis cumque, consectetur atque omnis sunt perspiciatis
-        voluptatem dolore rerum id dicta nesciunt iure tenetur tempore maiores.
-        Cum quisquam id voluptatibus. Ich spreche Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Error, nobis cumque, consectetur atque
-        omnis sunt perspiciatis voluptatem dolore rerum id dicta nesciunt iure
-        tenetur tempore maiores. Cum quisquam id voluptatibus. Ich spreche Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Error, nobis cumque,
-        consectetur atque omnis sunt perspiciatis voluptatem dolore rerum id
-        dicta nesciunt iure tenetur tempore maiores. Cum quisquam id
-        voluptatibus. Ich spreche Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Error, nobis cumque, consectetur atque omnis sunt
-        perspiciatis voluptatem dolore rerum id dicta nesciunt iure tenetur
-        tempore maiores. Cum quisquam id voluptatibus. Ich spreche Lorem ipsum
-        dolor sit amet consectetur adipisicing elit. Error, nobis cumque,
-        consectetur atque omnis sunt perspiciatis voluptatem dolore rerum id
-        dicta nesciunt iure tenetur tempore maiores. Cum quisquam id
-        voluptatibus.
-      </Modal>
-
       <Tabs />
     </div>
   )
--- src/components/Hero/Hero01.jsx
+++ src/components/Hero/Hero01.jsx
@@ -1,14 +1,31 @@
 import { GatsbyImage } from "gatsby-plugin-image"
 import React from "react"
+import useModal from "./../../hooks/useModal"
+import Button from "../Button/Button"
 import * as styles from "./Hero01.module.scss"
+import Modal from "../Modal/Modal"
 const Hero01 = ({ description, image }) => {
+  const { isOpen, handleStopPropagation, toggleModal, transitions } = useModal()
+
   return (
     <div className={styles.wrapper}>
       <div>
         <h3 dangerouslySetInnerHTML={{ __html: description }} />
+        <Button text="Reach out" onClick={toggleModal} />
       </div>
 
-      <div style={{ width: 200 }}>
+      <Modal
+        handleStopPropagation={handleStopPropagation}
+        transitions={transitions}
+        toggleModal={toggleModal}
+      >
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio, maxime.
+        Modi, nam provident, veritatis sed sit velit dicta saepe ratione quos
+        laudantium nostrum officiis qui minima similique inventore doloribus
+        quod!
+      </Modal>
+
+      <div style={{ width: 500 }}>
         <GatsbyImage image={image.gatsbyImageData} alt={description} />
       </div>
     </div>
